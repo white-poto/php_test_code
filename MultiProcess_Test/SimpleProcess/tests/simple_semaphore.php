a83dcd73698826c6fb2a9287dc9abba8e4235b6a
root <root@localhost.localdomain>
2014-08-23 20:13:30 +0800

    add swoole test

--- MultiProcess_Test/SimpleProcess/tests/simple_semaphore.php
+++ MultiProcess_Test/SimpleProcess/tests/simple_semaphore.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: Administrator
+ * User: huyanping
  * Date: 14-8-23
  * Time: 上午11:53
  */
---
+++ swoole_test/simple.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: huyanping
+ * Date: 14-8-23
+ * Time: 下午12:05
+ */
+

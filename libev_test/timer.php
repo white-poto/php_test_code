d3061f430de5be2ef02a92d2d503ff20454aa4db
Jenner <hypxm@qq.com>
2015-08-04 06:48:01 +0000

    test

--- libev_test/timer.php
+++ libev_test/timer.php
@@ -6,7 +6,7 @@
  * Time: 14:22
  */
 
-$w1 = new EvTimer(2, 0, function ($w) {
+$w1 = new EvTimer(10, 3, function ($w) {
     echo "Custom data: $w->data\n";
     echo "2 seconds elapsed\n";
 }, 'abcd');

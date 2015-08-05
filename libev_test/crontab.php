d4c91f33cfd26aaab82cf8a50196dc14101cb1b7
huyanping <hypxm@qq.com>
2015-08-05 14:38:54 +0800

    test

--- libev_test/crontab.php
+++ libev_test/crontab.php
@@ -6,7 +6,7 @@
  * Time: 14:34
  */
 
-$timer = new EvPeriodic(0., 0.0001, null, function($w, $revents){
+$timer = new EvPeriodic(0., 0.001, null, function($w, $revents){
     echo microtime(), PHP_EOL;
 });
 
